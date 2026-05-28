34a2535236945b6bc874cc6a524104df7f27ce26
Hot-Sweeper <hot.sweeper@hotmail.com>
2026-05-28 11:49:03 +0200

    fix: allow network dev origin

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["100.99.121.82"],
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "i.scdn.co" },
